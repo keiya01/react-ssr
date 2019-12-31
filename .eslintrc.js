a5226c14c0918f113634ac3864ce039ccd926100
keiya01 <keiya.s.0210@gmail.com>
2019-12-31 14:35:33 +0900

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,35 @@
 module.exports = {
-	env: {
-		browser: true,
-		node: true
-	},
-	parserOptions: {
-		sourceType: "module",
-		ecmaVersion: 2019,
-		ecmaFeatures: [ jsx ]
-	},
-	override: [
-		{
-			files: [ "*.ts", "*.tsx" ],
-			extends: [
-				"eslint:recommended",
-				"plugin:prettier/recommended",
-				"prettier/@typescript-eslint",
-				"plugin:@typescript-eslint/recommended",
-				"plugin:@typescript-eslint/eslint-recommended",
-				"plugin:react/recommended"
-			],
-			plugins: [ "@typescript-eslint", "react", "react-hooks" ],
-			parser: "@typescript-eslint/parser",
-			rules: {
-				"@typescript-eslint/no-empty-function": 0,
-				"@typescript-eslint/no-use-before-define": 0,
-				"@typescript-eslint/explicit-function-return-type": 0,
-				"@typescript-eslint/no-explicit-any": 0,
-				"react/display-name": 0,
-				"react/prop-types": 0
-			}
-		}
-	]
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2019,
+    ecmaFeatures: ["jsx"]
+  },
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:react/recommended"
+      ],
+      plugins: ["@typescript-eslint", "react", "react-hooks"],
+      parser: "@typescript-eslint/parser",
+      rules: {
+        "@typescript-eslint/no-empty-function": 0,
+        "@typescript-eslint/no-use-before-define": 0,
+        "@typescript-eslint/explicit-function-return-type": 0,
+        "@typescript-eslint/no-explicit-any": 0,
+        "react/display-name": 0,
+        "react/prop-types": 0,
+        "no-console": "error"
+      }
+    }
+  ]
 };
