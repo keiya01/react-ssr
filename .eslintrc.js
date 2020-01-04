c00bedc6c443830bbc6bcd32959f3458b53eca38
keiya01 <keiya.s.0210@gmail.com>
2020-01-04 17:40:42 +0900

    Update eslint to add no-var-requires rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
         "@typescript-eslint/no-use-before-define": 0,
         "@typescript-eslint/explicit-function-return-type": 0,
         "@typescript-eslint/no-explicit-any": 0,
+        "@typescript-eslint/no-var-requires": 0,
         "react/display-name": 0,
         "react/prop-types": 0,
         "no-console": "error"
