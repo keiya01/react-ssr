3193ae6e6567381afc18bd1b8e19981429098b63
keiya01 <keiya.s.0210@gmail.com>
2020-01-05 18:04:09 +0900

    Add redux

---
+++ src/client/redux/store.ts
@@ -0,0 +1,29 @@
+import { createStore, applyMiddleware } from "redux";
+import createSagaMiddleware, { SagaMiddleware } from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { rootReducer } from "./reducer";
+import { rootSaga } from "./saga";
+
+export const setStore = (preloadedState: Record<string, any> = {}) => {
+  const saga = createSagaMiddleware();
+  const middleware = applyMiddleware(saga);
+  const enhancer = composeWithDevTools(middleware);
+  const store = createStore(rootReducer, preloadedState, enhancer);
+  const runSaga = async () => {
+    return saga
+      .run(rootSaga)
+      .toPromise<SagaMiddleware<typeof runSaga>["run"]>();
+  };
+
+  if (process.env.IS_BROWSER) {
+    runSaga();
+  }
+
+  if (module.hot) {
+    module.hot.accept("./reducer", async () => {
+      store.replaceReducer(require("./reducer").rootReducer);
+    });
+  }
+
+  return { store, runSaga };
+};
