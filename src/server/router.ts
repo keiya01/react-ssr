6b4e7ebdeb94a7f01c8a7f4671689602b3866568
keiya01 <keiya.s.0210@gmail.com>
2020-01-06 16:27:35 +0900

    Add function to load service-worker and manifest.wabmanifest

--- src/server/router.ts
+++ src/server/router.ts
@@ -3,5 +3,14 @@ import * as renderer from "./controller/renderer";
 
 export const setRouter = (app: Express) => {
   app.use("/public", express.static("dist/client"));
+  app.use(
+    "/service-worker.js",
+    express.static("dist/client/service-worker.js")
+  );
+  app.use(
+    "/manifest.webmanifest",
+    express.static("dist/client/manifest.webmanifest")
+  );
+
   app.get("*", renderer.handleRender);
 };
