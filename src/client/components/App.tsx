edde116ac2629db96de98fab7582d36bcfa81886
keiya01 <keiya.s.0210@gmail.com>
2020-01-06 16:32:58 +0900

    Fix App component

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { State } from "../redux/reducer";
+import { ADD_COUNT } from "../redux/actions/count";
+import { ADD_COUNT_ASYNC } from "../redux/saga/countAsync";
 
 const App = () => {
+  const count = useSelector((state: State) => state.counter.count);
+  const dispatch = useDispatch();
+
+  const handleOnClick = () => {
+    dispatch({ type: ADD_COUNT });
+  };
+
+  const handleOnClickAsync = () => {
+    dispatch({ type: ADD_COUNT_ASYNC });
+  };
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <h1>Click below buttons</h1>
+      <p>{count}</p>
+      <button type="button" onClick={handleOnClick}>
+        Sync
+      </button>
+      <button type="button" onClick={handleOnClickAsync}>
+        Async
+      </button>
     </div>
   );
 };
